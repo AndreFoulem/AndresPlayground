a34d47aec1343124b0fd7f3428b3658dbd8d54ec
AndreFoulem <timmycasual@icloud.com>
2023-04-15 13:53:46 -0700

    Initial Commit

---
+++ AndresPlayground/AndresPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndresPlaygroundApp.swift
+//  AndresPlayground
+//
+//  Created by AndreMacBook on 2023-04-15.
+//
+
+import SwiftUI
+
+@main
+struct AndresPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LazyVGrid_Main()
+        }
+    }
+}
---
+++ AndresPlayground/LazyVGrid/LazyVGrid_Main.swift
@@ -0,0 +1,20 @@
+//
+//  LazyVGrid_Main.swift
+//  AndresPlayground
+//
+//  Created by AndreMacBook on 2023-04-15.
+//
+
+import SwiftUI
+
+struct LazyVGrid_Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LazyVGrid_Main_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyVGrid_Main()
+    }
+}
