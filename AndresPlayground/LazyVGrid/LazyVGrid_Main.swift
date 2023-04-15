b1a5fedf453bb37f7a8f6725ae6a308af7d0cfac
AndreFoulem <timmycasual@icloud.com>
2023-04-15 13:59:18 -0700

    Add a template LazyVGrid

--- AndresPlayground/LazyVGrid/LazyVGrid_Main.swift
+++ AndresPlayground/LazyVGrid/LazyVGrid_Main.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct LazyVGrid_Main: View {
+    let columns = [
+      GridItem(.fixed(50)),
+      GridItem(.fixed(50)),
+      GridItem(.fixed(50))
+    ]
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ScrollView {
+        LazyVGrid(columns: columns) {
+          ForEach(0..<100) { i in
+            Text("Item \(i)")
+              .frame(maxWidth: .infinity, maxHeight: .infinity)
+              .background(.yellow)
+          }
+        }
+      }
+      
     }
 }
 
