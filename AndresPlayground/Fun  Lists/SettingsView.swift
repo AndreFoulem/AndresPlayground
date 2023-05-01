55c7cf76f7f9906f11f7c7f1f8a89922f3135383
AndreFoulem <timmycasual@icloud.com>
2023-05-01 12:59:24 -0700

    Add enum

---
+++ AndresPlayground/Fun  Lists/SettingsView.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsView.swift
+//  AndresPlayground
+//
+//  Created by AndreMacBook on 2023-05-01.
+//
+
+import SwiftUI
+
+enum AppearanceStyle {
+  case dark
+  case light
+  case auto
+}
+
+struct SettingsView: View {
+ 
+  var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
