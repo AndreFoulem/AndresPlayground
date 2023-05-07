119167d371bcbb86f8c9b362c36e40f35bbe1598
AndreFoulem <timmycasual@icloud.com>
2023-05-07 13:43:30 -0700

    Implement all protocols for text and gesture

--- AndresPlayground/SwiftThinking_Advanced/protocols/ProtocolMainScreen.swift
+++ AndresPlayground/SwiftThinking_Advanced/protocols/ProtocolMainScreen.swift
@@ -7,33 +7,62 @@
 
 import SwiftUI
 
-struct DefaultColorTheme {
+
+protocol ColorThemeProtocol {
+  var primary: Color { get }
+  var secondary: Color { get }
+  var tertiary: Color { get }
+}
+
+protocol ButtonTextProtocol {
+  var buttonText: String { get }
+}
+
+protocol ButtonPressedProtocol {
+  func buttonPressed()
+}
+
+protocol ButtonDataSourceProtocol: ButtonTextProtocol, ButtonPressedProtocol {}
+
+struct DefaultColorTheme: ColorThemeProtocol {
   let primary: Color = .blue
   let secondary: Color = .white
   let tertiary: Color = .gray
 }
 
+class DefaultDataSource: ButtonDataSourceProtocol {
+  var buttonText: String = "Protocols are awesome"
+  func buttonPressed() {
+    print("Button was pressed")
+  }
+}
+
 struct ProtocolMainScreen: View {
   
-    let colorTheme: DefaultColorTheme = DefaultColorTheme()
+    let colorTheme: ColorThemeProtocol
+    let dataSource: ButtonDataSourceProtocol
   
     var body: some View {
       ZStack {
         colorTheme.tertiary
           .ignoresSafeArea()
         
-        Text("Protocols are awesome")
+        Text(dataSource.buttonText)
           .font(.headline)
           .foregroundColor(colorTheme.secondary)
           .padding()
           .background(colorTheme.primary)
           .cornerRadius(10)
+          .onTapGesture {
+            dataSource.buttonPressed()
+          }
       }
     }
 }
 
 struct ProtocolMainScreen_Previews: PreviewProvider {
     static var previews: some View {
-        ProtocolMainScreen()
+      ProtocolMainScreen(colorTheme: DefaultColorTheme(), dataSource: DefaultDataSource())
+      
     }
 }
