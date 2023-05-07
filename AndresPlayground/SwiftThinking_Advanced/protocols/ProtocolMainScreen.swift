b981a79788280d214c1b1021a995097641a9b877
AndreFoulem <timmycasual@icloud.com>
2023-05-07 12:45:58 -0700

    Add DefaultColorTheme struct

---
+++ AndresPlayground/SwiftThinking_Advanced/protocols/ProtocolMainScreen.swift
@@ -0,0 +1,26 @@
+//
+//  ProtocolMainScreen.swift
+//  AndresPlayground
+//
+//  Created by AndreMacBook on 2023-05-07.
+//
+
+import SwiftUI
+
+struct DefaultColorTheme {
+  let primary: Color = .blue
+  let secondary: Color = .white
+  let tertiary: Color = .gray
+}
+
+struct ProtocolMainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProtocolMainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ProtocolMainScreen()
+    }
+}
