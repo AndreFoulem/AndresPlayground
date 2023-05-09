c477be088ac7510a2b1ed55420e4d3192cca7d8f
AndreFoulem <timmycasual@icloud.com>
2023-05-09 15:22:23 -0700

    Add viewmodel and data service

--- AndresPlayground/SwiftThinking_Advanced/dependency injection/DependencyInjectionScreen.swift
+++ AndresPlayground/SwiftThinking_Advanced/dependency injection/DependencyInjectionScreen.swift
@@ -7,7 +7,26 @@
 
 import SwiftUI
 
+struct PostModel {
+  
+}
+
+class ProductionDataService {
+  
+}
+
+class DependencyInjectionScreenVM: ObservableObject {
+  @Published var dataArray: [PostModel] = []
+  
+  init() {}
+  
+  private func loadPoasts() {}
+}
+
+
 struct DependencyInjectionScreen: View {
+    @State private var vm = DependencyInjectionScreenVM()
+  
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
