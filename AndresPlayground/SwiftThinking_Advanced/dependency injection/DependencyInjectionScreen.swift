08c6d8618e4ce38ff6cb24fd1a7fe933acb558b0
AndreFoulem <timmycasual@icloud.com>
2023-05-09 15:18:39 -0700

    Add DependencyInjectionScreen

---
+++ AndresPlayground/SwiftThinking_Advanced/dependency injection/DependencyInjectionScreen.swift
@@ -0,0 +1,20 @@
+//
+//  DependencyInjectionScreen.swift
+//  AndresPlayground
+//
+//  Created by AndreMacBook on 2023-05-09.
+//
+
+import SwiftUI
+
+struct DependencyInjectionScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DependencyInjectionScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DependencyInjectionScreen()
+    }
+}
